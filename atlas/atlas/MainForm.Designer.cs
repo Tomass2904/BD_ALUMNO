bd68a9cabcdeec105291f6ff4715bcc67146c30f
Tomass2904 <116579627+Tomass2904@users.noreply.github.com>
2023-08-14 21:10:12 -0300

    ATLAS
    ATLAS

---
+++ atlas/atlas/MainForm.Designer.cs
@@ -0,0 +1,314 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Lab-01
+ * Date: 14/08/2023
+ * Time: 20:04
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace atlas
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.pic_5 = new System.Windows.Forms.PictureBox();
+			this.pic_8 = new System.Windows.Forms.PictureBox();
+			this.pic_7 = new System.Windows.Forms.PictureBox();
+			this.pic_6 = new System.Windows.Forms.PictureBox();
+			this.pic_1 = new System.Windows.Forms.PictureBox();
+			this.pic_2 = new System.Windows.Forms.PictureBox();
+			this.pic_3 = new System.Windows.Forms.PictureBox();
+			this.pic_4 = new System.Windows.Forms.PictureBox();
+			this.pic_central = new System.Windows.Forms.PictureBox();
+			this.lbl_marte = new System.Windows.Forms.Label();
+			this.lbl_mercurio = new System.Windows.Forms.Label();
+			this.lbl_urano = new System.Windows.Forms.Label();
+			this.lbl_venus = new System.Windows.Forms.Label();
+			this.lbl_Tierra = new System.Windows.Forms.Label();
+			this.lbl_saturno = new System.Windows.Forms.Label();
+			this.lbl_neptuno = new System.Windows.Forms.Label();
+			this.lbl_jupiter = new System.Windows.Forms.Label();
+			this.lbl_info = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pic_5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_8)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_7)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_6)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_central)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pic_5
+			// 
+			this.pic_5.BackColor = System.Drawing.Color.Transparent;
+			this.pic_5.Location = new System.Drawing.Point(638, 466);
+			this.pic_5.Name = "pic_5";
+			this.pic_5.Size = new System.Drawing.Size(128, 122);
+			this.pic_5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pic_5.TabIndex = 0;
+			this.pic_5.TabStop = false;
+			this.pic_5.Click += new System.EventHandler(this.func_click);
+			// 
+			// pic_8
+			// 
+			this.pic_8.BackColor = System.Drawing.Color.Transparent;
+			this.pic_8.Location = new System.Drawing.Point(1146, 466);
+			this.pic_8.Name = "pic_8";
+			this.pic_8.Size = new System.Drawing.Size(143, 122);
+			this.pic_8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pic_8.TabIndex = 1;
+			this.pic_8.TabStop = false;
+			this.pic_8.Click += new System.EventHandler(this.func_click);
+			// 
+			// pic_7
+			// 
+			this.pic_7.BackColor = System.Drawing.Color.Transparent;
+			this.pic_7.Location = new System.Drawing.Point(966, 466);
+			this.pic_7.Name = "pic_7";
+			this.pic_7.Size = new System.Drawing.Size(140, 122);
+			this.pic_7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pic_7.TabIndex = 2;
+			this.pic_7.TabStop = false;
+			this.pic_7.Click += new System.EventHandler(this.func_click);
+			// 
+			// pic_6
+			// 
+			this.pic_6.BackColor = System.Drawing.Color.Transparent;
+			this.pic_6.Location = new System.Drawing.Point(781, 466);
+			this.pic_6.Name = "pic_6";
+			this.pic_6.Size = new System.Drawing.Size(152, 122);
+			this.pic_6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pic_6.TabIndex = 3;
+			this.pic_6.TabStop = false;
+			this.pic_6.Click += new System.EventHandler(this.func_click);
+			// 
+			// pic_1
+			// 
+			this.pic_1.BackColor = System.Drawing.Color.Transparent;
+			this.pic_1.Location = new System.Drawing.Point(12, 466);
+			this.pic_1.Name = "pic_1";
+			this.pic_1.Size = new System.Drawing.Size(143, 122);
+			this.pic_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pic_1.TabIndex = 4;
+			this.pic_1.TabStop = false;
+			this.pic_1.Click += new System.EventHandler(this.func_click);
+			// 
+			// pic_2
+			// 
+			this.pic_2.BackColor = System.Drawing.Color.Transparent;
+			this.pic_2.Location = new System.Drawing.Point(169, 466);
+			this.pic_2.Name = "pic_2";
+			this.pic_2.Size = new System.Drawing.Size(140, 122);
+			this.pic_2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pic_2.TabIndex = 5;
+			this.pic_2.TabStop = false;
+			this.pic_2.Click += new System.EventHandler(this.func_click);
+			// 
+			// pic_3
+			// 
+			this.pic_3.BackColor = System.Drawing.Color.Transparent;
+			this.pic_3.Location = new System.Drawing.Point(327, 466);
+			this.pic_3.Name = "pic_3";
+			this.pic_3.Size = new System.Drawing.Size(152, 122);
+			this.pic_3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pic_3.TabIndex = 6;
+			this.pic_3.TabStop = false;
+			this.pic_3.Click += new System.EventHandler(this.func_click);
+			// 
+			// pic_4
+			// 
+			this.pic_4.BackColor = System.Drawing.Color.Transparent;
+			this.pic_4.Location = new System.Drawing.Point(495, 466);
+			this.pic_4.Name = "pic_4";
+			this.pic_4.Size = new System.Drawing.Size(128, 122);
+			this.pic_4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pic_4.TabIndex = 7;
+			this.pic_4.TabStop = false;
+			this.pic_4.Click += new System.EventHandler(this.func_click);
+			// 
+			// pic_central
+			// 
+			this.pic_central.BackColor = System.Drawing.Color.Transparent;
+			this.pic_central.Location = new System.Drawing.Point(12, 21);
+			this.pic_central.Name = "pic_central";
+			this.pic_central.Size = new System.Drawing.Size(611, 415);
+			this.pic_central.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pic_central.TabIndex = 8;
+			this.pic_central.TabStop = false;
+			this.pic_central.Click += new System.EventHandler(this.func_click);
+			// 
+			// lbl_marte
+			// 
+			this.lbl_marte.BackColor = System.Drawing.Color.Transparent;
+			this.lbl_marte.ForeColor = System.Drawing.SystemColors.ControlLight;
+			this.lbl_marte.Location = new System.Drawing.Point(1168, 605);
+			this.lbl_marte.Name = "lbl_marte";
+			this.lbl_marte.Size = new System.Drawing.Size(100, 23);
+			this.lbl_marte.TabIndex = 9;
+			this.lbl_marte.Text = "Marte";
+			// 
+			// lbl_mercurio
+			// 
+			this.lbl_mercurio.BackColor = System.Drawing.Color.Transparent;
+			this.lbl_mercurio.ForeColor = System.Drawing.SystemColors.ControlLight;
+			this.lbl_mercurio.Location = new System.Drawing.Point(978, 605);
+			this.lbl_mercurio.Name = "lbl_mercurio";
+			this.lbl_mercurio.Size = new System.Drawing.Size(100, 23);
+			this.lbl_mercurio.TabIndex = 10;
+			this.lbl_mercurio.Text = "Mercurio";
+			// 
+			// lbl_urano
+			// 
+			this.lbl_urano.BackColor = System.Drawing.Color.Transparent;
+			this.lbl_urano.ForeColor = System.Drawing.SystemColors.ControlLight;
+			this.lbl_urano.Location = new System.Drawing.Point(805, 605);
+			this.lbl_urano.Name = "lbl_urano";
+			this.lbl_urano.Size = new System.Drawing.Size(100, 23);
+			this.lbl_urano.TabIndex = 11;
+			this.lbl_urano.Text = "Urano";
+			// 
+			// lbl_venus
+			// 
+			this.lbl_venus.BackColor = System.Drawing.Color.Transparent;
+			this.lbl_venus.ForeColor = System.Drawing.SystemColors.ControlLight;
+			this.lbl_venus.Location = new System.Drawing.Point(651, 605);
+			this.lbl_venus.Name = "lbl_venus";
+			this.lbl_venus.Size = new System.Drawing.Size(100, 23);
+			this.lbl_venus.TabIndex = 12;
+			this.lbl_venus.Text = "Venus";
+			// 
+			// lbl_Tierra
+			// 
+			this.lbl_Tierra.BackColor = System.Drawing.Color.Transparent;
+			this.lbl_Tierra.ForeColor = System.Drawing.SystemColors.ControlLight;
+			this.lbl_Tierra.Location = new System.Drawing.Point(32, 605);
+			this.lbl_Tierra.Name = "lbl_Tierra";
+			this.lbl_Tierra.Size = new System.Drawing.Size(100, 23);
+			this.lbl_Tierra.TabIndex = 13;
+			this.lbl_Tierra.Text = "Tierra";
+			// 
+			// lbl_saturno
+			// 
+			this.lbl_saturno.BackColor = System.Drawing.Color.Transparent;
+			this.lbl_saturno.ForeColor = System.Drawing.SystemColors.ControlLight;
+			this.lbl_saturno.Location = new System.Drawing.Point(181, 605);
+			this.lbl_saturno.Name = "lbl_saturno";
+			this.lbl_saturno.Size = new System.Drawing.Size(100, 23);
+			this.lbl_saturno.TabIndex = 14;
+			this.lbl_saturno.Text = "Saturno";
+			// 
+			// lbl_neptuno
+			// 
+			this.lbl_neptuno.BackColor = System.Drawing.Color.Transparent;
+			this.lbl_neptuno.ForeColor = System.Drawing.SystemColors.ControlLight;
+			this.lbl_neptuno.Location = new System.Drawing.Point(349, 605);
+			this.lbl_neptuno.Name = "lbl_neptuno";
+			this.lbl_neptuno.Size = new System.Drawing.Size(100, 23);
+			this.lbl_neptuno.TabIndex = 15;
+			this.lbl_neptuno.Text = "Neptuno";
+			// 
+			// lbl_jupiter
+			// 
+			this.lbl_jupiter.BackColor = System.Drawing.Color.Transparent;
+			this.lbl_jupiter.ForeColor = System.Drawing.SystemColors.ControlLight;
+			this.lbl_jupiter.Location = new System.Drawing.Point(506, 605);
+			this.lbl_jupiter.Name = "lbl_jupiter";
+			this.lbl_jupiter.Size = new System.Drawing.Size(100, 23);
+			this.lbl_jupiter.TabIndex = 16;
+			this.lbl_jupiter.Text = "Jupiter";
+			// 
+			// lbl_info
+			// 
+			this.lbl_info.BackColor = System.Drawing.Color.Transparent;
+			this.lbl_info.Location = new System.Drawing.Point(651, 21);
+			this.lbl_info.Name = "lbl_info";
+			this.lbl_info.Size = new System.Drawing.Size(638, 415);
+			this.lbl_info.TabIndex = 17;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.ClientSize = new System.Drawing.Size(1319, 637);
+			this.Controls.Add(this.lbl_info);
+			this.Controls.Add(this.lbl_jupiter);
+			this.Controls.Add(this.lbl_neptuno);
+			this.Controls.Add(this.lbl_saturno);
+			this.Controls.Add(this.lbl_Tierra);
+			this.Controls.Add(this.lbl_venus);
+			this.Controls.Add(this.lbl_urano);
+			this.Controls.Add(this.lbl_mercurio);
+			this.Controls.Add(this.lbl_marte);
+			this.Controls.Add(this.pic_central);
+			this.Controls.Add(this.pic_4);
+			this.Controls.Add(this.pic_3);
+			this.Controls.Add(this.pic_2);
+			this.Controls.Add(this.pic_1);
+			this.Controls.Add(this.pic_6);
+			this.Controls.Add(this.pic_7);
+			this.Controls.Add(this.pic_8);
+			this.Controls.Add(this.pic_5);
+			this.Name = "MainForm";
+			this.Text = "atlas";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			((System.ComponentModel.ISupportInitialize)(this.pic_5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_8)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_7)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_6)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pic_central)).EndInit();
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Label lbl_info;
+		private System.Windows.Forms.Label lbl_jupiter;
+		private System.Windows.Forms.Label lbl_neptuno;
+		private System.Windows.Forms.Label lbl_saturno;
+		private System.Windows.Forms.Label lbl_Tierra;
+		private System.Windows.Forms.Label lbl_venus;
+		private System.Windows.Forms.Label lbl_urano;
+		private System.Windows.Forms.Label lbl_mercurio;
+		private System.Windows.Forms.Label lbl_marte;
+		private System.Windows.Forms.PictureBox pic_central;
+		private System.Windows.Forms.PictureBox pic_4;
+		private System.Windows.Forms.PictureBox pic_3;
+		private System.Windows.Forms.PictureBox pic_2;
+		private System.Windows.Forms.PictureBox pic_1;
+		private System.Windows.Forms.PictureBox pic_6;
+		private System.Windows.Forms.PictureBox pic_7;
+		private System.Windows.Forms.PictureBox pic_8;
+		private System.Windows.Forms.PictureBox pic_5;
+	}
+}
---
+++ atlas/atlas/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace atlas
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public string [] vector;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void func_cargarDatos(){
+			vector = new string [8];
+			vector[0]=@"e:/tierra.png";
+			vector[1]=@"e:/saturno.png";
+			vector[2]=@"e:/neptuno.png";
+			vector[3]=@"e:/jupiter.png";
+			vector[4]=@"e:/venus.png";
+			vector[5]=@"e:/urano.png";
+			vector[6]=@"e:/mercurio.png";
+			vector[7]=@"e:/marte.png";
+			pic_1.Image=Image.FromFile(vector[0]);
+			pic_2.Image=Image.FromFile(vector[1]);
+			pic_3.Image=Image.FromFile(vector[2]);
+			pic_4.Image=Image.FromFile(vector[3]);
+			pic_5.Image=Image.FromFile(vector[4]);
+			pic_6.Image=Image.FromFile(vector[5]);
+			pic_7.Image=Image.FromFile(vector[6]);
+			pic_8.Image=Image.FromFile(vector[7]);
+		}
+		void func_click (object sender, EventArgs e){
+			PictureBox pic_aux;
+			pic_aux = (PictureBox) sender;
+			pic_central.Image=pic_aux.Image;
+		}
+		
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			func_cargarDatos();
+		}
+	}
+}
